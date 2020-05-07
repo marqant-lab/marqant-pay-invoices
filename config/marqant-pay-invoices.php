f9484e6a624a8c1c0d4675e0a326074c79f980f9
vagrant <vagrant@homestead>
2020-05-07 11:34:13 +0200

    initial commit

---
+++ config/marqant-pay-invoices.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ |--------------------------------------------------------------------------
+ | MarqantPayInvoices Configuration
+ |--------------------------------------------------------------------------
+ |
+ | In this configuration file you will find all options to set for this
+ | package.
+ |
+ | See: https://github.com/marqant-lab/marqant-pay-invoices
+ |
+ */
+
+return [
+
+];
